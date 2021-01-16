380fad8609c63177de43cdce7284ce829fe99e3e
Adriana Bonilla-Ramirez <adrianalbr12@gmail.com>
2021-01-15 20:52:15 -0500

    added process.env to server.js

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/budget", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
   useNewUrlParser: true,
   useFindAndModify: false
 });
@@ -25,4 +25,4 @@ app.use(require("./routes/api.js"));
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
-});
+})
